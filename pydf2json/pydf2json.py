387c620bd69577c743a6a65f0f9b7eca1b873f93
xamiel <dont.look@my.privates>
2018-08-01 14:34:54 -0700

    Added LZW decoding
    Taken from opensource project pdfminer for the time being.

--- pydf2json/pydf2json.py
+++ pydf2json/pydf2json.py
@@ -41,6 +41,93 @@ class SpecViolation(Exception):
 class MaxSizeExceeded(Exception):
     pass
 
+class LZWDecoder(object):
+    # LZW code sourced from pdfminer
+    # Copyright (c) 2004-2009 Yusuke Shinyama <yusuke at cs dot nyu dot edu>
+    #
+    # Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+    # documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+    # the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+    # and to permit persons to whom the Software is furnished to do so, subject to the following conditions: etc...
+
+    # ** Modified code so importing StreamIO wasn't needed.
+
+    def __init__(self, fp):
+        self.fp = fp
+        self.fpos = 0
+        self.buff = 0
+        self.bpos = 8
+        self.nbits = 9
+        self.table = None
+        self.prevbuf = None
+        return
+
+    def readbits(self, bits):
+        v = 0
+        while 1:
+            # the number of remaining bits we can get from the current buffer.
+            r = 8-self.bpos
+            if bits <= r:
+                # |-----8-bits-----|
+                # |-bpos-|-bits-|  |
+                # |      |----r----|
+                v = (v<<bits) | ((self.buff>>(r-bits)) & ((1<<bits)-1))
+                self.bpos += bits
+                break
+            else:
+                # |-----8-bits-----|
+                # |-bpos-|---bits----...
+                # |      |----r----|
+                v = (v<<r) | (self.buff & ((1<<r)-1))
+                bits -= r
+                if self.fpos == len(self.fp):
+                    raise EOFError
+                x = self.fp[self.fpos]
+                self.fpos += 1
+                if not x: raise EOFError
+                self.buff = ord(x)
+                self.bpos = 0
+        return v
+
+    def feed(self, code):
+        x = ''
+        if code == 256:
+            self.table = [ chr(c) for c in range(256) ] # 0-255
+            self.table.append(None) # 256
+            self.table.append(None) # 257
+            self.prevbuf = ''
+            self.nbits = 9
+        elif code == 257:
+            pass
+        elif not self.prevbuf:
+            x = self.prevbuf = self.table[code]
+        else:
+            if code < len(self.table):
+                x = self.table[code]
+                self.table.append(self.prevbuf+x[0])
+            else:
+                self.table.append(self.prevbuf+self.prevbuf[0])
+                x = self.table[code]
+            l = len(self.table)
+            if l == 511:
+                self.nbits = 10
+            elif l == 1023:
+                self.nbits = 11
+            elif l == 2047:
+                self.nbits = 12
+            self.prevbuf = x
+        return x
+
+    def run(self):
+        while 1:
+            try:
+                code = self.readbits(self.nbits)
+            except EOFError:
+                break
+            x = self.feed(code)
+            yield x
+        return
+
 class PyDF2JSON(object):
 
     # password: Use this password to decrypt document.
@@ -1571,6 +1658,8 @@ class PyDF2JSON(object):
         known_encoders = {
             'FlateDecode',
             'Fl',
+            'LZWDecode',
+            'LZW',
             'ASCIIHexDecode',
             'AHx',
             'ASCII85Decode',
@@ -1578,8 +1667,6 @@ class PyDF2JSON(object):
             'Crypt'
         }
         known_not_implemented = {
-            'LZWDecode',
-            'LZW',
             'RunLengthDecode',
             'RL',
             'JBIG2Decode',
@@ -1606,6 +1693,8 @@ class PyDF2JSON(object):
             if filter in known_encoders:
                 if filter == 'FlateDecode' or filter == 'Fl':
                     new_stream = self.__flatedecode(new_stream)
+                if filter == 'LZWDecode' or filter == 'LZW':
+                    new_stream = self.__lzw_decode(new_stream)
                 if filter == 'ASCIIHexDecode' or filter == 'AHx':
                     new_stream = self.__asciihexdecode(new_stream)
                 if filter == 'ASCII85Decode' or filter == 'A85':
@@ -1847,6 +1936,10 @@ class PyDF2JSON(object):
                 dec_str = dec_str[:-1]
 
         return dec_str
+
+
+    def __lzw_decode(self, data):
+        return ''.join(LZWDecoder(data).run())
 
 
     def __asciihexdecode(self, my_stream):
